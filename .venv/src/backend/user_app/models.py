b658428cf2c0a713842ab94694090e8874eb12f7
Brandon Nesslage <Brandonnesslage@gmail.com>
2024-08-06 15:36:49 -0400

    Initial Commit for DND app

---
+++ .venv/src/backend/user_app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+
+#TODO  I would really love to add the ability to not only accept email as login but also username or password.
+
+
+
+# Inheriting from 'AbstractUser' lets us use all the fields of the default User,
+# and overwrite the fields we need to change
+# This is different from 'AbstractBaseUser', which only gets the password management features from the default User,
+# and needs the developer to define other relevant fields.
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    phone_number = PhoneNumberField(unique=True)
+    username = models.CharField(max_length=16, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    # notice the absence of a "Password field", that is built in.
+    # django uses the 'username' to identify users by default, but many modern applications use 'email' instead
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'phone_number'] # Email & Password are required by default.
+
+    def __str__(self):
+        return self.email
