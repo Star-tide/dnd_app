69c7ec835e8a379d9bef5ae20efe65fa5dfd44d9
Brandon Nesslage <Brandonnesslage@gmail.com>
2024-08-12 12:15:24 -0400

    Fixed Super user, added google_app

---
+++ .venv/src/backend/dnd_proj/dnd_proj/debug_stuff.py
@@ -0,0 +1,4 @@
+{
+  "trainer": "bness@email.com",
+  "token": "294ceb0be6db8b2c8b43c4c342401e225e3199c5"
+}
---
+++ .venv/src/backend/dnd_proj/user_app/views.py
@@ -0,0 +1,63 @@
+from django.contrib.auth import authenticate
+from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_404_NOT_FOUND,
+    HTTP_204_NO_CONTENT,
+)
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class Sign_up(APIView):
+    def post(self, request):
+        request.data["username"] = request.data["email"]
+        user = User.objects.create_user(**request.data)
+        token = Token.objects.create(user=user)
+        return Response(
+            {"trainer": user.email, "token": token.key}, status=HTTP_201_CREATED
+        )
+    
+
+
+class Log_in(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        user = authenticate(username=email, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key, "user": user.email})
+        else:
+            return Response("No user matching credentials", status=HTTP_404_NOT_FOUND)
+        
+class Info(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"email": request.user.email})
+    
+class Log_out(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+    
+class Master_Sign_Up(APIView):
+    def post(self, request):
+        data = request.data.copy()
+        data["username"] = request.data.get("email")
+        master_user = User.objects.create_user(**data)
+        master_user.is_staff = True
+        master_user.is_superuser = True
+        master_user.save()
+        token = Token.objects.create(user=master_user)
+        return Response(
+            {"master_user": master_user.email, "token": token.key}, status=HTTP_201_CREATED
+        )
